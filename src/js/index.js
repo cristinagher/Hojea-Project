cfadfee5d27a46d718e5bee1303bfab3d68fbf4c
cristinagher <cristinagamezhernandez@gmail.com>
2024-05-27 21:39:38 +0200

    update

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,28 @@
 // El styles lo importamos aquí, ya se carga después al compilar todo
 import '../scss/styles.scss';
+
+
+const nav = document.getElementById('header-container');
+window.addEventListener("scroll",function(){
+  if(document.documentElement.scrollTop > 20){
+    nav.classList.add("sticky");
+  }
+  else{
+    nav.classList.remove("sticky");
+  }
+})
+
+
+const buttonMenuElement = document.getElementById('button-menu');
+const menuElement = document.getElementById('menu');
+
+function handleMenu() {
+if (menuElement.classList.contains('show')) {
+    menuElement.classList.remove('show')
+}
+else {
+    menuElement.classList.add('show');
+}
+}
+
+buttonMenuElement.addEventListener('click', handleMenu);
